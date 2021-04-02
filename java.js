d65ab3cbac50885bb80736fc187c37103f3abedc
Yann Melo <yann.melo@gmail.com>
2021-04-02 17:44:13 -0300

    Botão fechar pedido

--- java.js
+++ java.js
@@ -1,4 +1,4 @@
-
+let valorAtual = 0; 
 
 function selectfood(eproduct){
     const select = document.querySelector(".food .border");
@@ -7,6 +7,8 @@ function selectfood(eproduct){
     }
     const select1 = document.querySelector(eproduct);
     select1.classList.add("border")
+    valorAtual = valorAtual + 1;
+    button3();
 }
 function selectdrink(eproduct){
     const select = document.querySelector(".drinks .border");
@@ -15,6 +17,8 @@ function selectdrink(eproduct){
     }
     const select1 = document.querySelector(eproduct);
     select1.classList.add("border")
+    valorAtual = valorAtual + 1;
+    button3();
 }
 function selectextra(eproduct){
     const select = document.querySelector(".extra .border");
@@ -23,14 +27,17 @@ function selectextra(eproduct){
     }
     const select1 = document.querySelector(eproduct);
     select1.classList.add("border")
+    valorAtual = valorAtual + 1;
+    button3();
 }
+function button3(){
+    if (valorAtual === 3){ 
+        const teste = document.querySelector(".bottom .button h1");
+        teste.innerHTML = "Fechar pedido"
+    }
+    if (valorAtual === 3){
+        const teste2 = document.querySelector(".button")
+        teste2.classList.add("close")
+    }
+} 
 
-
-
-
-
-/*
-const hidden = document.querySelector(".ebox ion-icon");
-hidden.classList.toggle("positionicon")
-<ion-icon name="checkmark-circle"></ion-icon>
-*/
