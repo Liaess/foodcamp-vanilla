8ab0c04e54221a9506b130089e2862dd3f41e65f
Yann Melo <yann.melo@gmail.com>
2021-04-05 14:51:16 -0300

    Green box

--- java.js
+++ java.js
@@ -92,20 +92,35 @@ function finish(){
     const linkurl = `https://wa.me/5521987972806?text=${texturi}`;
     window.location.href = linkurl; 
 }
-/*
+
 function confirm(){
+    pricefood = pricefood.replace("R$ ","");
+    pricefood = pricefood.replace(",",".");
+    pricefood = parseFloat(pricefood);
+    pricedrink = pricedrink.replace("R$ ","");
+    pricedrink = pricedrink.replace(",",".");
+    pricedrink = parseFloat(pricedrink);
+    priceextra = priceextra.replace("R$ ","");
+    priceextra = priceextra.replace(",",".");
+    priceextra = parseFloat(priceextra);
+    totalprice = (pricefood + pricedrink + priceextra).toFixed(2);
+
+
     const test = document.querySelector(".confirm .foodname");
-    test.innerHTML = <p>${foodchoice}</p>
+    test.innerHTML = foodchoice
     const test1 = document.querySelector(".confirm .foodprice");
-    test1.innerHTML = <p>${pricefood}</p>
+    test1.innerHTML = pricefood
 
     const test2 = document.querySelector(".confirm .drinkname");
-    test2.innerHTML = <p>${drinkchoice}</p>
+    test2.innerHTML = drinkchoice
     const test3 = document.querySelector(".confirm .drinkprice");
-    test3.innerHTML = <p>${pricedrink}</p>
+    test3.innerHTML = pricedrink
     
     const test4 = document.querySelector(".confirm .extraname");
-    test4.innerHTML = <p>${extrachoice}</p>
+    test4.innerHTML = extrachoice
     const test5 = document.querySelector(".confirm .extraprice");
-    test5.innerHTML = <p>${priceextra}</p>
-} */
+    test5.innerHTML = priceextra
+    
+    const test6 = document.querySelector(".confirm .totalprice");
+    test6.innerHTML = 'R$ ' + totalprice
+} 
